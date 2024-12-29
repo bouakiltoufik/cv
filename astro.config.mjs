30739b0d2d7153271fb4a03e37f27279cdeb04b1
Toufik BOUAKIL <bouakiltoufik@gmail.com>
2024-12-29 13:47:19 +0100

    mettre à jour la configuration du site et du chemin de base dans astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bouakiltoufik.github.io',
-  base: 'cv',
-  trailingSlash: 'always',
+  site: 'https://bouakiltoufik.github.io/cv/', // Notez le "/" final
+  base: '/cv/', // Le chemin de base doit commencer et se terminer par "/"
+  trailingSlash: 'always', // Assurez-vous que ce paramètre correspond à vos besoins
   integrations: [tailwind()],
 });
